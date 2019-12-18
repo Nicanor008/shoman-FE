ba055b70574ca5726ee0eff7aab1e72eaf512482
Nicanor008 <nicanorkorir008@gmail.com>
2019-12-18 15:53:58 +0300

    Auth designs

---
+++ src/components/input.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { withStyles } from "@material-ui/core/styles"
+import InputLabel from "@material-ui/core/InputLabel"
+import FormControl from "@material-ui/core/FormControl"
+import InputBase from "@material-ui/core/InputBase"
+import InputComponent from "../input"
+
+const BootstrapInput = withStyles(theme => ({
+    root: {
+      "label + &": {
+        marginTop: theme.spacing(3),
+      },
+    },
+    input: {
+      borderRadius: 4,
+      position: "relative",
+      backgroundColor: theme.palette.background.paper,
+      border: "1px solid #ced4da",
+      fontSize: 16,
+      padding: "10px 26px 10px 12px",
+      transition: theme.transitions.create(["border-color", "box-shadow"]),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(","),
+      "&:focus": {
+        borderRadius: 4,
+        borderColor: "#80bdff",
+        boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+      },
+    },
+  }))(InputBase)
+
+const InputComponent = (props) => {
+//   const classes = useStyles()
+  return (
+    <FormControl className={props.classes}>
+      <InputLabel htmlFor="demo-customized-textbox">Email Address</InputLabel>
+      <BootstrapInput
+        id="demo-customized-textbox"
+        type="email"
+        autoComplete
+        autoFocus
+      />
+    </FormControl>
+  )
+}
+
+export default InputComponent
--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,99 +1,91 @@
-import React from 'react';;
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Layout from '../../components/layout';
-import SEO from '../../components/seo';
+import React from "react"
+import PropTypes from "prop-types"
+import { makeStyles } from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+import { Grid } from "@material-ui/core"
+import LoginComponent from "../../components/auth/login"
+import SignUpComponent from "../../components/auth/signup"
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props
 
   return (
     <Typography
       component="div"
       role="tabpanel"
       hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
       {value === index && <Box p={3}>{children}</Box>}
     </Typography>
-  );
+  )
 }
 
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.any.isRequired,
   value: PropTypes.any.isRequired,
-};
+}
 
 function a11yProps(index) {
   return {
-    id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
-  };
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  }
 }
 
 const useStyles = makeStyles(theme => ({
   root: {
+    flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-    width: 500,
   },
-}));
+}))
 
-export default function FullWidthTabs() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
+export default function AuthPage() {
+  const classes = useStyles()
+  const [value, setValue] = React.useState(0)
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = index => {
-    setValue(index);
-  };
+    setValue(newValue)
+  }
 
   return (
     <Layout>
-    <SEO title="Auth" />
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          Item Three
-        </TabPanel>
-      </SwipeableViews>
-    </div>
+      <SEO title="Auth" />
+      <div className={classes.root}>
+        <center>
+          <Grid>
+            <Grid item xs={12} md={4}>
+              <AppBar position="static">
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="simple tabs example"
+                >
+                  <Tab label="Login" {...a11yProps(0)} />
+                  <Tab label="Sign Up" {...a11yProps(1)} />
+                </Tabs>
+              </AppBar>
+            </Grid>
+          </Grid>
+
+          <TabPanel value={value} index={0}>
+            <LoginComponent />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <SignUpComponent />
+          </TabPanel>
+        </center>
+      </div>
     </Layout>
-  );
+  )
 }
