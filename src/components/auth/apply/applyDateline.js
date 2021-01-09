0f274e85c294e77e8133fcb965dc3045cfd52d96
Nicanor008 <nicanorkorir008@gmail.com>
2021-01-09 19:47:06 +0300

    Date changes

--- src/components/auth/apply/applyDateline.js
+++ src/components/auth/apply/applyDateline.js
@@ -4,8 +4,8 @@ import React from "react"
 export default function ApplyDateline({ classname }) {
   return (
     <p className={classname}>
-      Application closes <b>5th Janury 2021</b>.Mentorship starts{" "}
-      <b>11th Janury, 2021.</b>
+      Application closes <b>5th February 2021</b>.Mentorship starts{" "}
+      <b>15th February, 2021.</b>
       <br />
       Be sure to have read <Link href="/about" target="_blank">about</Link> section to understand
       the requirements
